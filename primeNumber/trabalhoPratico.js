eb6dbc91b329141f0ee208cf6cf22ca5517e0d0c
Leise Goi <leisegoi@gmail.com>
2021-03-15 15:10:48 -0300

    Add files to primeNumber project

---
+++ primeNumber/trabalhoPratico.js
@@ -0,0 +1,21 @@
+var numeroTeste = 997;
+var resultado = [];
+var somaPrimos = [];
+
+for(i=1; i<=numeroTeste; i++){
+    if(numeroTeste%i == 0){
+        resultado.push(i);
+    }
+}
+
+
+console.log(resultado);
+
+if(resultado.length > 2 || numeroTeste == 1) {
+    console.log(numeroTeste + " não é um número primo");
+} else {
+    console.log(numeroTeste + " é um número primo");
+}
+
+
+
